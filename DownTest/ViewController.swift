c7a5e94cafce0968d5b4bf418ca21d155217152e
Mitch Stanley <mitch@fastmail.co.uk>
2021-01-30 11:18:42 +0000

    Initial Commit

--- DownTest/ViewController.swift
+++ DownTest/ViewController.swift
@@ -6,13 +6,17 @@
 //
 
 import Cocoa
+import Down
 
-class ViewController: NSViewController {
+class TestViewController: NSViewController {
 
+    @IBOutlet weak var customView: NSView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        renderDownInWebView()
     }
 
     override var representedObject: Any? {
@@ -24,3 +28,20 @@ class ViewController: NSViewController {
 
 }
 
+private extension TestViewController {
+    
+    func renderDownInWebView() {
+        let readMeURL = Bundle.main.url(forResource: nil, withExtension: "md")!
+        let readMeContents = try! String(contentsOf: readMeURL)
+        debugPrint(readMeContents)
+        do {
+            let downView = try DownView(frame: view.bounds, markdownString: readMeContents, didLoadSuccessfully: {
+                print("Markdown was rendered.")
+            })
+            downView.autoresizingMask = [.width, .height]
+            view.addSubview(downView, positioned: .below, relativeTo: nil)
+        } catch {
+            NSApp.presentError(error)
+        }
+    }
+}
