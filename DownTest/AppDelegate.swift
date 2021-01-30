4e05440dd208c5a1bc4ae1dbb591ae5d8f011f7a
Mitch Stanley <mitch@fastmail.co.uk>
2021-01-30 11:06:49 +0000

    Initial Commit

---
+++ DownTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DownTest
+//
+//  Created by Mitch Stanley on 30/01/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DownTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DownTest
+//
+//  Created by Mitch Stanley on 30/01/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
